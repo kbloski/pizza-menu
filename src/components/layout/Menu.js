03ecf9ef89ebcfe9c965cd820e357d88849cbf48
Kamil Błoński <kblonski02@gmail.com>
2025-01-04 10:34:30 +0100

    update

--- src/components/layout/Menu.js
+++ src/components/layout/Menu.js
@@ -3,16 +3,22 @@ import pizzaData from '../../storage/data.js'
 import Pizza from "../Pizza.js"
 
 export default function Menu() {
+    const pizzas = [...pizzaData];
+
     return <main className='menu'>
         <h2>Our menu</h2>
 
-        <ul className="pizzas">
-            {pizzaData.map(
-                (pizza, index) =>  
-                    <Pizza key={index} pizzaObj={pizza}></Pizza> 
-                )
-            }
-        </ul>
+        {
+            !!pizzas.length ?
+            <ul className="pizzas">
+                {!!pizzas.length && pizzas.map(
+                    (pizza, index) =>  
+                        <Pizza key={index} pizzaObj={pizza}></Pizza> 
+                    )
+                }
+            </ul>
+            : <p>We are still working on our menu!</p>
+    }
 
     </main>
 }

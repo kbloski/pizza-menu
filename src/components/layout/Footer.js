5bd9dc162e840966eff32c496dbc0b7121ccc7ac
Kamil Błoński <kblonski02@gmail.com>
2025-01-04 10:29:45 +0100

    update

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -2,13 +2,18 @@ import './Footer.css'
 
 export default function Footer() {
     const hour = new Date().getHours();
-    const openHour = 12;
+    const openHour = 10;
     const closeHour = 22;
     const isOpen = openHour <= hour && hour < closeHour;
 
     return <footer className="footer">
         <p>© Fast React Pizza Co.</p>
-        { isOpen }
-        { isOpen ? <p>We are open!</p> : <p>We are closed!</p> }
+        { !isOpen ? <p>We are closed!</p> : <div className='order'>
+                <p>
+                    We are open until {closeHour} PM. Hurry up!
+                </p>
+                <button className='btn'>Order now</button>
+            </div>
+        }
     </footer>
 }
